d4fa62bfca4aadcf8b017037dadcd78064761f72
Kristoffer Vassbø <kristoffervassbo@gmail.com>
2024-03-27 19:01:02 +0100

    add many actions

--- src/actions.js
+++ src/actions.js
@@ -1,11 +1,84 @@
+const indexInput = { id: 'index', type: 'number', label: 'Index', default: 1, min: 1, max: 5000 }
+const valueInput = { id: 'value', type: 'textinput', label: 'Name' }
+const idInput = { id: 'id', type: 'textinput', label: 'ID', tooltip: 'Check the FreeShow configs to find ids' }
+const volumeInput = { id: 'volume', type: 'number', label: 'Volume', default: 1, min: 0, max: 1, step: 0.1 }
+const gainInput = { id: 'gain', type: 'number', label: 'Gain', default: 0, min: 0, max: 1, step: 0.1 }
+const transitionInputs = [
+	{
+		type: 'dropdown',
+		label: 'Text or Media?',
+		id: 'id',
+		default: 'text',
+		choices: [
+			{ id: 'text', label: 'Text' },
+			{ id: 'media', label: 'Media' },
+		],
+	},
+	{
+		type: 'dropdown',
+		label: 'Type',
+		id: 'type',
+		default: 'fade',
+		choices: [
+			{ id: 'none', label: 'None' },
+			{ id: 'blur', label: 'Blur' },
+			{ id: 'fade', label: 'Fade' },
+			{ id: 'crossfade', label: 'Crossfade' },
+			{ id: 'fly', label: 'Fly' },
+			{ id: 'scale', label: 'Scale' },
+			{ id: 'spin', label: 'Spin' },
+		],
+	},
+	{ id: 'duration', type: 'number', label: 'Duration (ms)', default: 500, min: 0 },
+	{ id: 'easing', type: 'textinput', label: 'Easing', default: 'sine' },
+]
+const variableName = {
+	id: 'name',
+	type: 'textinput',
+	label: 'Variable name',
+	tooltip: 'The name of the variable that should be changed',
+}
+const variableInputs = [
+	variableName,
+	{ id: 'value', type: 'textinput', label: 'Value', tooltip: 'Keep empty to toggle on/off' },
+	{
+		type: 'dropdown',
+		label: 'Action',
+		id: 'variableAction',
+		default: '',
+		choices: [
+			{ id: '', label: 'None' },
+			{ id: 'increment', label: 'Increment' },
+			{ id: 'decrement', label: 'Decrement' },
+		],
+	},
+]
+
 module.exports = function (self) {
 	const actionData = {
+		// PROJECT
+		index_select_project: { name: 'Select project by index', options: [indexInput] },
+		next_project_item: { name: 'Next project item' },
+		previous_project_item: { name: 'Previous project item' },
+		index_select_project_item: { name: 'Select project item by index', options: [indexInput] },
+
+		// SHOWS
+		name_select_show: { name: 'Select show by name', options: [valueInput] },
+
+		// PRESENTATION
 		next_slide: { name: 'Next slide' },
 		previous_slide: { name: 'Previous slide' },
-		next_project_show: { name: 'Next show' },
-		previous_project_show: { name: 'Previous show' },
-		// goto_group: {name: "Go to group", options},
+		index_select_slide: { name: 'Select slide by index', options: [indexInput] },
+		name_select_slide: { name: 'Select slide by name', options: [valueInput] },
+		lock_output: { name: 'Toggle output lock' },
+		toggle_output_windows: { name: 'Toggle output windows' },
+		id_select_group: { name: 'Select slide group by ID', options: [idInput] },
 
+		// STAGE
+		id_select_stage_layout: { name: 'Select stage layout by ID', options: [idInput] },
+
+		// CLEAR
+		restore_output: { name: 'Restore output' },
 		clear_all: { name: 'Clear all' },
 		clear_background: { name: 'Clear background' },
 		clear_slide: { name: 'Clear slide' },
@@ -13,21 +86,24 @@ module.exports = function (self) {
 		clear_audio: { name: 'Clear audio' },
 		clear_next_timer: { name: 'Clear next slide timer' },
 
-		// change_output_style // Change output style
+		// OVERLAYS
+		index_select_overlay: { name: 'Toggle overlay by index', options: [indexInput] },
+		name_select_overlay: { name: 'Toggle overlay by name', options: [valueInput] },
 
-		// index_select_project // Select project by index
-		// index_select_project_show // Select project item by index
-		index_select_slide: {
-			name: 'Select slide by index',
+		// AUDIO
+		change_volume: { name: 'Change audio volume', options: [volumeInput, gainInput] },
+
+		// VISUAL
+		id_select_output_style: { name: 'Select output style by ID', options: [idInput] },
+		change_transition: { name: 'Change transition', options: transitionInputs },
+
+		// OTHER
+		change_variable: { name: 'Change variable', options: variableInputs },
+		custom_message: {
+			name: 'Custom API message',
 			options: [
-				{
-					id: 'index',
-					type: 'number',
-					label: 'Slide index',
-					default: 1,
-					min: 1,
-					max: 1000,
-				},
+				{ id: 'id', type: 'textinput', label: 'API ID' },
+				{ ...valueInput, tooltip: 'Format as stringified JSON. E.g: {"value": "hi"}' },
 			],
 		},
 	}
@@ -55,7 +131,13 @@ module.exports = function (self) {
 
 		// format data
 		let action = event.actionId
-		let data = JSON.stringify({ ...event.options, action })
+		let data = { ...event.options, action }
+
+		// custom message
+		if (action === 'custom_message') {
+			action = data.id
+			data = { ...JSON.parse(data.value || '{}'), action }
+		}
 
 		// log action
 		let options = Object.keys(event.options).length ? ` + ${JSON.stringify(event.options)}` : ''
--- src/presets.js
+++ src/presets.js
@@ -13,19 +13,24 @@ module.exports = function (self) {
 			text: '❯',
 			bgcolor: combineRgb(5, 115, 50),
 		},
-		next_project_show: {
-			name: 'Next show',
+		next_project_item: {
+			name: 'Next project item',
 			text: '⬇️',
 			bgcolor: combineRgb(0, 25, 60),
 		},
-		previous_project_show: {
-			name: 'Previous show',
+		previous_project_item: {
+			name: 'Previous project item',
 			text: '⬆️',
 			bgcolor: combineRgb(0, 25, 60),
 		},
 		clear_all: {
 			name: 'Clear all',
 			text: '✖',
+			bgcolor: combineRgb(100, 8, 8),
+		},
+		lock_output: {
+			name: 'Toggle output lock',
+			text: '🔒',
 			bgcolor: combineRgb(100, 8, 8),
 		},
 
@@ -64,15 +69,67 @@ module.exports = function (self) {
 		},
 	}
 
+	const groups = {
+		break: { name: 'Break', color: combineRgb(245, 37, 94) },
+		bridge: { name: 'Bridge', color: combineRgb(245, 37, 152) },
+		chorus: { name: 'Chorus', color: combineRgb(245, 37, 210) },
+		intro: { name: 'Intro', color: combineRgb(213, 37, 245) },
+		outro: { name: 'Outro', color: combineRgb(165, 37, 245) },
+		pre_chorus: { name: 'Pre Chorus', color: combineRgb(136, 37, 245) },
+		tag: { name: 'Tag', color: combineRgb(117, 37, 245) },
+		verse: { name: 'Verse', color: combineRgb(88, 37, 245) },
+	}
+	// GENERATE GROUPS
+	for (let i = 0; i < Object.keys(groups).length; i++) {
+		const groupId = Object.keys(groups)[i]
+		const group = groups[groupId]
+		presetData['id_select_group_' + groupId] = {
+			category: 'Groups',
+			id: 'id_select_group',
+			name: `Switch between groups of this type`,
+			text: group.name,
+			bgcolor: group.color,
+			options: { id: groupId },
+		}
+	}
+
+	// GENERATE TRANSITIONS
+	const transitions = ['None', 'Blur', 'Fade', 'Crossfade', 'Fly', 'Scale', 'Spin']
+	for (let i = 0; i < transitions.length; i++) {
+		const transition = transitions[i]
+		presetData['change_transition_' + i] = {
+			category: 'Transitions',
+			id: 'change_transition',
+			name: `Set transition to ${transition}%`,
+			text: `${transition}`,
+			bgcolor: combineRgb(0, 50, i * 10 + 10),
+			options: { id: 'text', type: transition.toLowerCase() },
+		}
+	}
+
+	// GENERATE VOLUME LEVELS
+	const volumes = [0, 25, 50, 75, 100]
+	for (let i = 0; i < volumes.length; i++) {
+		const volume = volumes[i]
+		presetData['change_volume_' + i] = {
+			category: 'Volume',
+			id: 'change_volume',
+			name: `Set volume to ${volume}%`,
+			text: `${volume}%`,
+			bgcolor: combineRgb(volume + 20, 0, volume),
+			options: { volume: volume / 100 },
+		}
+	}
+
 	// GENERATE SLIDE INDEXES
 	for (let i = 1; i <= 30; i++) {
 		presetData['index_select_slide_' + i] = {
 			category: 'Slides',
 			id: 'index_select_slide',
 			name: 'Select slide at index ' + i,
-			options: { index: i },
 			text: i.toString(),
 			bgcolor: combineRgb(20, 0, 20),
+			options: { index: i },
 		}
 	}
 
